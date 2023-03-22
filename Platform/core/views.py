1c1fe0470b16a04809b5ce53de27ee92bafe40c5
hytangs <64950573+hytangs@users.noreply.github.com>
2023-03-22 15:52:01 +0800

    Basic UI and layout

--- Platform/core/views.py
+++ Platform/core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+
+def homepage(request):
+    return render(request, "index.html")
