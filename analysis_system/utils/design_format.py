d72f830485bffb87cfe0299a24c2274c63be6a5d
Edmund <e0406924@u.nus.edu>
2023-04-09 14:25:35 +0800

    format and docstring

--- analysis_system/utils/design_format.py
+++ analysis_system/utils/design_format.py
@@ -1,9 +1,21 @@
 import streamlit as st
 
-# Function to align text to column width. Allowed alignments: "justify", "center", "right", "left"
 def align_text(text, alignment):
+    '''Function to align text to column width. Allowed alignments: "justify", "center", "right", "left"
+    Args:
+        text (str): Text to be aligned
+        alignment (str): Alignment of text
+        
+    Returns:
+        str: Aligned text
+    '''
+
     return st.markdown(f'<div style="text-align: {alignment};">{text}</div>', unsafe_allow_html=True)
 
-# Insert Horizontal Line
 def horizontal_line():
+    '''Insert Horizontal Line
+    Returns:
+        str: Horizontal Line
+    '''
+
     return st.markdown("""---""")
