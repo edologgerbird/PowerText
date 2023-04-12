156998a718027485f2fe82aeeb23829c958b7c03
hytangs <64950573+hytangs@users.noreply.github.com>
2023-04-12 18:35:36 +0800

    Add results output

--- Platform/core/views.py
+++ Platform/core/views.py
@@ -1,6 +1,36 @@
+import random
+
+import numpy as np
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-
+@csrf_exempt
 def homepage(request):
-    return render(request, "index.html")
+    context = {}
+    if request.method == 'POST':
+        if request.POST['form_id'] == 'text_submit':
+            text = request.POST['comment']
+            predict = wrapper_random_guess(text)
+            context['text_submitted'] = text
+            context['label_hate'] = '1' if predict[0] == 1 else '0'
+            context['label_private'] = '1' if predict[1] == 1 else '0'
+            context['label_sexual'] = '1' if predict[2] == 1 else '0'
+            context['label_imperson'] = '1' if predict[3] == 1 else '0'
+            context['label_illegal'] = '1' if predict[4] == 1 else '0'
+            context['label_ads'] = '1' if predict[5] == 1 else '0'
+            context['label_ai'] = '1' if predict[6] == 1 else '0'
+    return render(request, "index.html", context)
+
+# helper functions
+def wrapper_all_zero(text):
+    return np.zeros(7)
+
+def wrapper_random_guess(text):
+    output = []
+    for i in range(7):
+        output.append(random.choice([0, 1]))
+    return output
+
+def wrapper_hate_bert(text):
+    pass
