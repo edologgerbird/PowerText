d095a1d4cbb0adbbcca70050fa73d273b4916b4d
hytangs <64950573+hytangs@users.noreply.github.com>
2023-04-13 00:28:53 +0800

    Add user_demo page for display

--- Platform/core/views.py
+++ Platform/core/views.py
@@ -13,6 +13,8 @@ def homepage(request):
         if request.POST['form_id'] == 'text_submit':
             text = request.POST['comment']
             predict = wrapper_hate_bert(text)
+
+            # render labels: possible implementation
             context['text_submitted'] = text
             context['label_hate'] = '1' if predict[0] == 1 else '0'
             context['label_private'] = '1' if predict[1] == 1 else '0'
@@ -21,7 +23,44 @@ def homepage(request):
             context['label_illegal'] = '1' if predict[4] == 1 else '0'
             context['label_ads'] = '1' if predict[5] == 1 else '0'
             context['label_ai'] = '1' if predict[6] == 1 else '0'
-    return render(request, "index.html", context)
+
+            # render text output: possible implementation
+            context['displayed_text'] = shrink_text(text)
+            flagged = '0'
+            violations = []
+            if predict[0] == 1:
+                flagged = '1'
+                violations.append('Hate')
+            elif predict[1] == 1:
+                flagged = '1'
+                violations.append('Privacy')
+            elif predict[2] == 1:
+                flagged = '1'
+                violations.append('Sexual')
+            elif predict[3] == 1:
+                flagged = '1'
+                violations.append('Impersonation')
+            elif predict[4] == 1:
+                flagged = '1'
+                violations.append('Illegal')
+            elif predict[5] == 1:
+                flagged = '1'
+                violations.append('Advertisement')
+            elif predict[6] == 1:
+                flagged = '1'
+                violations.append('AI Generated')
+
+            if flagged == '0':
+                message = 'The post passed text screening.'
+            else:
+                message = 'Detected '
+                for i in violations:
+                    message = message + i + ", "
+                message = message[:-2] + " Content!"
+            context['text_flagged'] = flagged
+            context['text_message'] = message
+
+    return render(request, "user_demo.html", context)
 
 # helper functions
 def wrapper_all_zero(text):
@@ -35,11 +74,16 @@ def wrapper_random_guess(text):
 
 def wrapper_hate_bert(text):
     predict = run_hate_bert(text)
-    print(predict)
     output = []
+    threshold = max(predict) / 2
     for i in predict:
-        if i > 5:
+        if i >= threshold:
             output.append(1)
         else:
             output.append(0)
     return output
+
+def shrink_text(text):
+    if len(text) >= 15:
+        text = text[:6] + "..." + text[-6:]
+    return text
